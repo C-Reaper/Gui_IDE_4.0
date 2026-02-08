9c97356a957a31a9c1dd09ed7253d1eedca88628
codeleaded <gnu.c.reaper@gmail.com>
2026-02-08 18:37:40 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -1,16 +1,27 @@
 #include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/Explorer.h"
 #include "/home/codeleaded/System/Static/Library/TextEditor.h"
 
-
+Explorer ex;
 TextEditor editor;
 
 void Resize(AlxWindow* w){
-    editor.rect.p.x = (float)GetWidth() * 0.1f;
+    ex.r.p.x = (float)GetWidth() * 0.01f;
+    ex.r.p.y = (float)GetHeight() * 0.01f;
+    ex.r.d.x = (float)GetWidth() * (0.2f - 0.01f * 2);
+    ex.r.d.y = (float)GetHeight() * (1.0f - 0.01f * 2);
+
+    editor.rect.p.x = (float)GetWidth() * 0.21f;
     editor.rect.p.y = (float)GetHeight() * 0.01f;
-    editor.rect.d.x = (float)(GetWidth() - editor.rect.p.x) * 0.99f;
-    editor.rect.d.y = (float)(GetHeight() - editor.rect.p.y) * 0.99f;
+    editor.rect.d.x = (float)GetWidth() * (0.8f - 0.01f * 2);
+    editor.rect.d.y = (float)GetHeight() * (1.0f - 0.01f * 2);
 }
 void Setup(AlxWindow* w){
+    ex = Explorer_New("..",Rect_New(
+		(Vec2){ 0.0f,0.0f },
+		(Vec2){ 0.0f,0.0f }
+	));
+
     editor = TextEditor_New(Rect_New(
         (Vec2){ 0.0f,0.0f },
         (Vec2){ 0.0f,0.0f }
@@ -20,14 +31,26 @@ void Setup(AlxWindow* w){
     Resize(w);
 }
 void Update(AlxWindow* w){
+	if(Stroke(ALX_MOUSE_L).PRESSED){
+		Explorer_File* ef = Explorer_Select(&ex,w->Strokes,GetMouse());
+		if(ef && Files_isFile(ef->path)){
+			printf("[Explorer_File]: Open -> '%s'\n",ef->path);
+            TextEditor_Load(&editor,ef->path);
+		}
+	}
+    
+    Explorer_Update(&ex,w->Strokes,GetMouse());
+    
     TextEditor_Update(&editor,w->Strokes,GetMouse());
     TextEditor_DefaultReact(&editor);
 
 	Clear(DARK_GRAY);
 
+	Explorer_Render(&ex,WINDOW_STD_ARGS);
 	TextEditor_Render(&editor,WINDOW_STD_ARGS);
 }
 void Delete(AlxWindow* w){
+	Explorer_Free(&ex);
 	TextEditor_Free(&editor);
 }
 
