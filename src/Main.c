cdac051a47eafab862e3b59eca6d2957293402f9
codeleaded <gnu.c.reaper@gmail.com>
2026-07-24 01:39:09 +0200

    update

--- src/Main.c
+++ src/Main.c
@@ -1,4 +1,4 @@
-#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/WindowEngine.h"
 #include "/home/codeleaded/System/Static/Library/Explorer.h"
 #include "/home/codeleaded/System/Static/Library/TextEditor.h"
 
